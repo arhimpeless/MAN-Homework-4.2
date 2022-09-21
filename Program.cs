6e55a9108d93aba59222604a8bc1dc4c31b19843
Andrey Markov <arherower@outlook.com>
2022-09-21 13:41:41 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Задание_4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+            int n;
+            Random rnd = new Random();
+            while (true)
+            {
+                Console.WriteLine("Введите два целых одинаковых числа. ");
+                Console.Write("Введите M: ");
+                m = int.Parse(Console.ReadLine());
+                Console.Write("Введите N: ");
+                n = int.Parse(Console.ReadLine());
+                int[,] array1 = new int[m, n];  //задали размерность нашего вводимого массива
+                int[,] array2 = new int[m, n];  //задали размерность для зеркального массива
+                if (m == n)
+                {
+                    for (int x = 0; x < m; x++)     //перебор по х
+                    {
+                        for (int y = 0; y < n; y++)  //перебор по y
+                        {
+                            array1[x, y] = rnd.Next(1, 20);
+                            Console.Write(array1[x, y]);
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("Отзеркаливаем относительно главной диагонали: ");
+                    for (int x = 0; x < m; x++)
+                    {
+                        for (int y = 0; y < n; y++)
+                        {
+                            array2[x, y] = array1[(n - 1) - x, y];
+                            Console.Write(array2[x, y]);
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("Процесс завершён!");
+                    Console.ReadKey();
+                }
+                if (m != n)
+                    { 
+                    Console.WriteLine("Неквадратно!\n");
+                    }
+            }
+        }
+    }
+}
